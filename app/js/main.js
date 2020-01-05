a8b41c20a2e158cc38f913ad9f20e3226588c534
Artem <gradysnn92@yandex.ru>
2020-01-05 23:37:01 +0300

    add style main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
-
+$('.banner-slider').slick({
+arrows: false,
+dots: true,
+});
+$('.product-slider').slick({
+  speed: 600,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  });
 });
