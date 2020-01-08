df9d05870d7daf3ae1d1d5c9d48fce6238fb322e
Artem <gradysnn92@yandex.ru>
2020-01-08 14:37:21 +0300

    Add auto catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function(){
+
 $('.banner-slider').slick({
 arrows: false,
 dots: true,
 });
+
 $('.product-slider').slick({
   speed: 600,
   slidesToShow: 6,
@@ -10,4 +12,16 @@ $('.product-slider').slick({
   arrows: false,
   dots: true,
   });
+
+  $('.btn-catalog').click(function() {
+    $('.nav').toggleClass('open')
+  });
+  $('.btn-catalog').click(function() {
+    $('.breadcrumb-item').toggleClass('open')
+  });
+/* $(window).scroll(function() {
+  $('.footer-sup').css( "border-top", "1px solid red" );
+}); */
+
+
 });
