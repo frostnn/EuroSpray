aaaea48641ff673994658aa9943215c6d910454a
Artem <gradysnn92@yandex.ru>
2020-01-10 08:59:47 +0300

    Add new item semi

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,13 @@ $('.product-slider').slick({
   arrows: false,
   dots: true,
   });
-
+  $('.new-slider').slick({
+    speed: 600,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    });
   $('.btn-catalog').click(function() {
     $('.nav').toggleClass('open')
   });
@@ -22,6 +28,18 @@ $('.product-slider').slick({
 /* $(window).scroll(function() {
   $('.footer-sup').css( "border-top", "1px solid red" );
 }); */
+$('.btn-product__porular').click(function() {
+  $('.product-slider').toggle();
+  $('.new-slider').hide();
+  $('.btn-product__porular').toggleClass('color');
+  $('.btn-product__new.color').toggleClass( 'color');
+ 
+});
 
-
+$('.btn-product__new').click(function() {
+  $('.new-slider').toggle();
+  $('.product-slider').hide();
+  $('.btn-product__new').toggleClass('color');
+  $('.btn-product__porular.color').toggleClass( 'color');
+});
 });
