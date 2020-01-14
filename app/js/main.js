2bae4ef45359e68501f8d9968611ab42bf558f01
Artem <gradysnn92@yandex.ru>
2020-01-14 16:42:02 +0300

    add page item and form of communication

--- app/js/main.js
+++ app/js/main.js
@@ -77,3 +77,30 @@ $(window).scroll(function(){
 });
 });
 
+$(document).ready(function(){
+  $(window).scroll(function(){
+    $('.block__title').addClass('on');
+    $('.scroll-effect').addClass('on');
+  });
+  });
+
+  $(document).ready(function() {
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+      var th = $(this);
+      $.ajax({
+        type: "POST",
+        url: "mail.php", //Change
+        data: th.serialize()
+      }).done(function() {
+        alert("Ваше письмо отправлено!");
+        setTimeout(function() {
+          // Done Functions
+          th.trigger("reset");
+        }, 1000);
+      });
+      return false;
+    });
+  
+  });
